0fedde54a8c5c17b47728d9ded5281b658f3dbfe
JacobOlesen <jacob.bank.olesen@gmail.com>
2023-05-25 12:17:25 +0200

    JS debugged

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("aktiv");
+    current[0].className = current[0].className.replace(" aktiv", "");
+    this.className += " aktiv";
+  });
+}
+
+
+
 //bruger koden new Date() til at hente data, som repræsenterer den aktuelle dato og tid.
-const d = new Date();
+let d = new Date();
 
 //Derefter bruges getDay() metoden på dette objekt til at hente den aktuelle ugedag (hvor 0 = søndag, 1 = mandag, 2 = tirsdag, og så videre).
 let day = d.getDay()
-document.getElementsByClassName("dayOutput").innerHTML = day;
 
 //Hvis getDay() er 1, får mandag ændret fontWeight til bold, det samme sker med alle andre ugedage
 // === kaldes også "strict equality" fordi det ikke kun sammenligner værdierne, men også typerne af de sammenlignede værdier. 
@@ -35,7 +79,6 @@ if (day === 0) {
   document.getElementById("søn").style.fontFamily = "nitti-typewriter-cameo";
   document.getElementById("søn1").style.fontFamily = "nitti-typewriter-cameo";
 }
-
 
 
 // Java til nedtælling 
@@ -114,3 +157,21 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("aktiv");
+    current[0].className = current[0].className.replace(" aktiv", "");
+    this.className += " aktiv";
+  });
+}
+
+
+
+
+// ----------- Filter funktion på menu siden - Jacob ---------- //
+
+
